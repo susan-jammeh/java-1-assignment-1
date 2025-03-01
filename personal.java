a2bfb687ee5f15c2e2faf339c9641c2fc944f7ed
susan-jammeh <jammehsusan220@gmail.com>
2025-02-28 18:02:28 -1100

    new commit

---
+++ personal.java
@@ -0,0 +1,35 @@
+public class personal {
+    public static void main(String[] arg) {
+
+        String name = "Susan Jammeh";
+
+        int age = 19;
+
+        String address = "kunkujang jattaya";
+
+        String marriage = "False";
+
+        String nationality = "Gambian";
+
+        String gender = "Female";
+
+        String school = "MDI";
+
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+        System.out.println("address:" + address);
+        System.out.println("marriage:" + marriage);
+        System.out.println("nationality:" + nationality);
+        System.out.println("gender:" + gender);
+        System.out.println("school:" + school);
+
+        int num = 10;
+        int number = 2;
+
+        System.out.println(num + number);
+        System.out.println(num * number);
+        System.out.println(num - number);
+        System.out.println(num / number);
+    }
+}
+
